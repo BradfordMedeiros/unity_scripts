460e5f1ed4119f54aff93f27026ab1fee9ba38bb
Brad <bradmedeiros0@gmail.com>
2019-01-22 23:04:10 -0800

    input moves rigid body, camera follow script

---
+++ MoveRelativeTo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRelativeTo : MonoBehaviour {
+
+   public GameObject gameObjectToFollow;
+   private Vector3 offset;
+   void Start () {
+     offset = transform.position - gameObjectToFollow.transform.position;
+   }
+	
+   // Update is called once per frame
+   void LateUpdate () {
+     transform.position = gameObjectToFollow.transform.position + offset;
+   }
+}
