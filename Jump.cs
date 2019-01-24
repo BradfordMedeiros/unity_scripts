aee8c3b2b7600d4de24c3ffc1dc0003c22c1b27e
Brad <bradmedeiros0@gmail.com>
2019-01-24 01:45:28 -0800

    simple jump script

---
+++ Jump.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+    private Rigidbody gameobject;
+    private Vector3 jump;
+    private float jumpForce = 2.0f;
+
+    public bool isGrounded;
+    Rigidbody rb;
+    void Start()
+    {
+        Cursor.visible = false;
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+}
